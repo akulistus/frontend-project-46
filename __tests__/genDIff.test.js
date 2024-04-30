0a5ae025c1c25285bd83b94ede24aac7420a677f
Eugeny <akulistus555@gmail.com>
2024-04-30 14:30:40 +0300

    fix

--- __tests__/genDIff.test.js
+++ __tests__/genDIff.test.js
@@ -1,7 +1,6 @@
-import genDiff from "../bin/genDiff.js";
-import fs from 'node:fs'
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import genDiff from '../bin/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,42 +11,41 @@ let file2;
 let resultDiff;
 
 test('test Plain diff', () => {
-  //DELETE
+  // DELETE
   file1 = getFixturePath('delete/deletePlainFile1.json');
   file2 = getFixturePath('delete/deletePlainFile2.json');
   resultDiff = genDiff(file1, file2);
   expect(resultDiff).toEqual('{\n  - proxy: 123.234.53.22\n}');
 
-  //ADD
+  // ADD
   file1 = getFixturePath('add/addPlainFile1.json');
   file2 = getFixturePath('add/addPlainFile2.json');
   resultDiff = genDiff(file1, file2);
   expect(resultDiff).toEqual('{\n  + host: hexlet.io\n}');
 
-  //CHANGE
+  // CHANGE
   file1 = getFixturePath('change/changePlainFile1.json');
   file2 = getFixturePath('change/changePlainFile2.json');
   resultDiff = genDiff(file1, file2);
   expect(resultDiff).toEqual('{\n  - timeout: 50\n  + timeout: 70\n}');
 
-  //NOCHANGE
+  // NOCHANGE
   file1 = getFixturePath('nochange/nochangePlainFile1.json');
   file2 = getFixturePath('nochange/nochangePlainFile2.json');
   resultDiff = genDiff(file1, file2);
   expect(resultDiff).toEqual('{\n    follow: false\n}');
 
-  //ALL
+  // ALL
   file1 = getFixturePath('all/allPlainFile1.json');
   file2 = getFixturePath('all/allPlainFile2.json');
-  const result = 
-  `{
+  const result = `{
     host: hexlet.io
   - timeout: 50
   + timeout: 20
   - proxy: 123.234.53.22
   - follow: false
   + verbose: true
-}`
+}`;
   resultDiff = genDiff(file1, file2);
   expect(resultDiff).toEqual(result);
 });
